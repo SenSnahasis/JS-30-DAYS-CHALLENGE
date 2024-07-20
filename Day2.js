997d7ea2dfa267b92578f5c9ac8283a531c02506
SenSnahasis <snahasis1001@gmail.com>
2024-07-21 01:15:20 +0530

    Day 2: Operators

---
+++ Day2.js
@@ -0,0 +1,92 @@
+/* Activity 1: Arithmetic Operations */
+// Task 1
+value1 = 10
+value2 = 20
+console.log(value1+value2);
+
+// Task 2
+value1 = 50
+value2 = 20
+console.log(value1-value2);
+
+// Task 3
+value1 = 10
+value2 = 20
+console.log(value1*value2);
+
+// Task 4
+value1 = 100
+value2 = 20
+console.log(value1/value2);
+
+// Task 5
+value1 = 110
+value2 = 20
+console.log(value1%value2);
+
+
+/* Activity 2: Assignment Operators */
+// Task 6
+value1 = 5
+value2 = 20
+value2 += value1
+console.log(value2);
+
+// Task 7
+value1 = 5
+value2 = 20
+value2 -= value1
+console.log(value2);
+
+
+/* Activity 3: Comparison Operators */
+// Task 8
+value1 = 5
+value2 = 20
+console.log(value1<value2);
+console.log(value1>value2);
+
+// Task 9
+value1 = 21
+value2 = 20
+value3 = 20
+console.log(value1>=value2);
+console.log(value2>=value3);
+console.log(value3<=value2);
+console.log(value3<=value1);
+
+// Task 10
+value1 = 5
+value2 = '5'
+console.log(value1 == value2);
+console.log(value1 === value2);
+
+
+/* Activity 4: Logical Operators */
+// Task 11
+value1 = '20'
+value2 = 20
+console.log(value1<=value2 && value1==value2);
+console.log(value1<=value2 && value1===value2);
+
+// Task 12
+value1 = 5
+value2 = '5'
+console.log(value1<=value2 || value1==value2);
+console.log(value1<=value2 || value1===value2);
+console.log(value1<value2 && value1===value2);
+
+// Task 13
+value1 = 5
+value2 = '5'
+console.log(!(value1 === value2));
+
+
+/* Activity 5: Ternary Operator */
+// Task 14
+value1 = 5
+result = value1>0 ? "Positive" : "Negative"
+console.log(result)
+
+
+
