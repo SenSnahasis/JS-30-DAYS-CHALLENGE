857c1e7cf66a068d86f144ee1cbc74a2539491bf
SenSnahasis <snahasis1001@gmail.com>
2024-07-21 00:40:47 +0530

    Day 1: Variables and Data Types

---
+++ Day1.js
@@ -0,0 +1,48 @@
+/* Variable Declaration */
+// Task 1
+var variableOne = 212;
+console.log(variableOne);
+
+// Task 2
+let variableTwo = "Snahasis"
+console.log(variableTwo);
+
+
+/* Constant Declaration */
+// Task 3
+const variableThree = true;
+console.log(variableThree);
+
+
+/* Data Types */
+// Task 4
+name = "Alex"
+roll = 202
+isActive = true
+obj = {
+    address: "Dum Dum 700030",
+    phone: 629484843
+}
+arr = ['Golu','Samrat','Rista','Tania','Shipi']
+console.log(typeof(name));
+console.log(typeof(roll));
+console.log(typeof(isActive));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+
+/* Reassigning Variables */
+// Task 5
+let username = "Riya"
+console.log(username);
+username = "Diya"
+console.log(username);
+
+
+/* Understanding const */
+// Task 6
+const value = 20
+console.log(value)
+value = 212
+console.log(value)
+
